2a58233e16245d07641b6fbbc1ce04c0a180805e
Ainar Garipov <A.Garipov@AdGuard.COM>
2022-10-28 21:21:09 +0300

    netutil: add more from agdns

--- netutil/addr_example_test.go
+++ netutil/addr_example_test.go
@@ -67,3 +67,51 @@ func ExampleSubdomains() {
 	//
 	// []string(nil)
 }
+
+func ExampleIsSubdomain() {
+	fmt.Printf("%-14s: %5t\n", "same domain", netutil.IsSubdomain("sub.example.com", "example.com"))
+	fmt.Printf("%-14s: %5t\n", "not immediate", netutil.IsSubdomain("subsub.sub.example.com", "example.com"))
+
+	fmt.Printf("%-14s: %5t\n", "empty", netutil.IsSubdomain("", ""))
+	fmt.Printf("%-14s: %5t\n", "same", netutil.IsSubdomain("example.com", "example.com"))
+	fmt.Printf("%-14s: %5t\n", "dot only", netutil.IsSubdomain(".example.com", "example.com"))
+	fmt.Printf("%-14s: %5t\n", "backwards", netutil.IsSubdomain("example.com", "sub.example.com"))
+	fmt.Printf("%-14s: %5t\n", "other domain", netutil.IsSubdomain("sub.example.com", "example.org"))
+	fmt.Printf("%-14s: %5t\n", "similar 1", netutil.IsSubdomain("sub.myexample.com", "example.org"))
+	fmt.Printf("%-14s: %5t\n", "similar 2", netutil.IsSubdomain("sub.example.com", "myexample.org"))
+
+	// Output:
+	// same domain   :  true
+	// not immediate :  true
+	// empty         : false
+	// same          : false
+	// dot only      : false
+	// backwards     : false
+	// other domain  : false
+	// similar 1     : false
+	// similar 2     : false
+}
+
+func ExampleIsImmediateSubdomain() {
+	fmt.Printf("%-14s: %5t\n", "same domain", netutil.IsImmediateSubdomain("sub.example.com", "example.com"))
+
+	fmt.Printf("%-14s: %5t\n", "empty", netutil.IsImmediateSubdomain("", ""))
+	fmt.Printf("%-14s: %5t\n", "same", netutil.IsImmediateSubdomain("example.com", "example.com"))
+	fmt.Printf("%-14s: %5t\n", "dot only", netutil.IsImmediateSubdomain(".example.com", "example.com"))
+	fmt.Printf("%-14s: %5t\n", "backwards", netutil.IsImmediateSubdomain("example.com", "sub.example.com"))
+	fmt.Printf("%-14s: %5t\n", "other domain", netutil.IsImmediateSubdomain("sub.example.com", "example.org"))
+	fmt.Printf("%-14s: %5t\n", "not immediate", netutil.IsImmediateSubdomain("subsub.sub.example.com", "example.com"))
+	fmt.Printf("%-14s: %5t\n", "similar 1", netutil.IsSubdomain("sub.myexample.com", "example.org"))
+	fmt.Printf("%-14s: %5t\n", "similar 2", netutil.IsSubdomain("sub.example.com", "myexample.org"))
+
+	// Output:
+	// same domain   :  true
+	// empty         : false
+	// same          : false
+	// dot only      : false
+	// backwards     : false
+	// other domain  : false
+	// not immediate : false
+	// similar 1     : false
+	// similar 2     : false
+}
